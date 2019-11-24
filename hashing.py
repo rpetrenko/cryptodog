65852ff8253118d0bf4e7d30642d630d7db34857
Roman Petrenko <rpetrenko@gmail.com>
2019-11-24 00:23:15 -0800

    add linux hash password

--- hashing.py
+++ hashing.py
@@ -1,4 +1,6 @@
 import hashlib
+from passlib.hash import sha512_crypt
+
 
 msg = b"HELLO"
 
@@ -26,3 +28,17 @@ msg = "P@sw0rd"
 m = hashlib.new('md4')
 m.update(msg.encode('utf-16le'))
 print("Windows: [{}]; hashed(md4): {}".format(msg, m.hexdigest()))
+
+# linux passwords (check /etc/login.defs for the ENCRYPT_METHOD and the number of rounds, 5000 default)
+# then check output of /etc/shadow
+# split string by : and take first two elements
+# first element is username, second is pass
+# split pass by $ symbol and take the second element, that will be salt
+# the first element is the password type, usually equals to 6
+# use this salt + rounds + password to can hashed password shown in /etc/shadow
+salt = "ABC"
+password = msg
+print("Salt={}, password={}".format(salt, password))
+hashed = sha512_crypt.using(salt=salt, rounds=5000).hash(password)
+print("Linux hashed password: {}".format(hashed))
+
